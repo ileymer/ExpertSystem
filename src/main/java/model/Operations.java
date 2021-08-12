0a0c989ac819bae48055c5a536a4f9ffd88538f7
Your Name <you@example.com>
2021-08-12 08:41:02 +0300

    tristate operations

--- src/main/java/model/Operations.java
+++ src/main/java/model/Operations.java
@@ -2,41 +2,69 @@ package model;
 
 public class Operations {
 
-    public static boolean Not(boolean f)
+    public static Tristate Not(Tristate f)
     {
-        return (!f);
+        if (f != Tristate.UNDEF)
+            return f == Tristate.FALSE ? Tristate.TRUE : Tristate.FALSE;
+        return f;
     }
 
-    public static boolean And(boolean f, boolean f1)
+    public static Tristate And(Tristate f1, Tristate f2)
     {
-        f = f && f1;
-        return (f);
+        if (f1 == Tristate.UNDEF || f2 == Tristate.UNDEF) {
+            return Tristate.UNDEF;
+        }
+        else if (f1 == Tristate.TRUE && f2 == Tristate.TRUE) {
+            return Tristate.TRUE;
+        }
+        return Tristate.FALSE;
     }
 
-    public static boolean Or(boolean f, boolean f1)
+    public static Tristate Or(Tristate f1, Tristate f2)
     {
-        f = f || f1;
-        return (f);
+        if (f1 == Tristate.TRUE || f2 == Tristate.TRUE) {
+            return Tristate.TRUE;
+        }
+        if (f1 == Tristate.UNDEF || f2 == Tristate.UNDEF) {
+            return Tristate.UNDEF;
+        }
+        return Tristate.FALSE;
     }
 
-    public static boolean Xor(boolean f, boolean f1)
+    public static Tristate Xor(Tristate f, Tristate f1)
     {
-        if (f != f1)
-            return true;
-        return false;
+        if (f == Tristate.UNDEF || f1 == Tristate.UNDEF) {
+            return Tristate.UNDEF;
+        }
+        if (f == f1) {
+            return Tristate.FALSE;
+        }
+        return Tristate.TRUE;
     }
 
-    public static boolean Imp(boolean f, boolean f1)
-    {
-        if (f == true && f1 == false)
-            return false;
-        return true;
+    public static Tristate Imp(Tristate f, Tristate f1) {
+        if (f == Tristate.TRUE) {
+            if (f1 == Tristate.FALSE) {
+                return Tristate.FALSE;
+            } else if (f1 == Tristate.UNDEF) {
+                return Tristate.FALSE;
+            }
+        } else if (f == Tristate.UNDEF) {
+            if (f1 == Tristate.UNDEF || f1 == Tristate.FALSE) {
+                return Tristate.UNDEF;
+            }
+        }
+        return Tristate.TRUE;
     }
 
-    public static boolean Eqv(boolean f, boolean f1)
+    public static Tristate Eqv(Tristate f, Tristate f1)
     {
-        if (f == f1)
-            return true;
-        return false;
+        if (f == Tristate.UNDEF || f1 == Tristate.UNDEF) {
+            return Tristate.UNDEF;
+        }
+        if (f == f1) {
+            return Tristate.TRUE;
+        }
+        return Tristate.FALSE;
     }
 }
